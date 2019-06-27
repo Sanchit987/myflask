37a942cdc8e8d17159589118055ced1e0bd7121d
Sanchit987 <kherasanchit1@gmail.com>
2019-06-27 12:03:41 +0530

    commit: seprate template file

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,7 @@
+from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    #return "Hello , world"
     user = {'username':'sanchit'}
-    return '''
-    <html>
-    <head>
-    <title>Home page - microblog </title>
-    </head>
-    <body bgcolor = 'cyan'>
-    <h1> hello , ''' + user['username'] + '''</h1>
-    </body>
-    </html>'''
+    return render_template('index.html',title = 'home',user = user)
