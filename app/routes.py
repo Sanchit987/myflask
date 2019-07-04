24981f8781c4c91a488998d79270e212dbbac3f3
Sanchit987 <kherasanchit1@gmail.com>
2019-07-04 12:48:07 +0530

    master1 commit1

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,5 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username':'sanchit'}
-    return render_template('index.html',title = 'home',user = user)
+    posts = [{'a':{'username':'ashish'},'body':'What a Beautiful day'},{'a':{'username':'suraj'},'body':'excited to learn something new'}]
+    return render_template('index.html',title = 'home',user = user,posts = posts)
