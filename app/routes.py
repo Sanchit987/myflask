3e6fe090bd8666e1e90ee91310f61c2cf1bcee83
Sanchit987 <kherasanchit1@gmail.com>
2019-07-05 12:21:51 +0530

    1

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,4 @@ def index():
     user = {'username':'sanchit'}
     posts = [{'a':{'username':'ashish'},'body':'What a Beautiful day'},{'a':{'username':'suraj'},'body':'excited to learn something new'}]
     return render_template('index.html',title = 'home',user = user,posts = posts)
+    
